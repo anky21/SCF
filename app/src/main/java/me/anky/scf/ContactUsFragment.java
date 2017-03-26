408b937658cdb8b85230f020124a23a307ea76bf
Anky <anky_au@hotmail.com>
2017-03-26 22:09:36 +1000

    feat: Create Contact Us fragment

--- app/src/main/java/me/anky/scf/ContactUsFragment.java
+++ app/src/main/java/me/anky/scf/ContactUsFragment.java
@@ -1,11 +1,17 @@
 package me.anky.scf;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -13,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class ContactUsFragment extends Fragment {
 
+    @BindView(R.id.compose_email_tv)
+    TextView mComposeEmailTv;
 
     public ContactUsFragment() {
         // Required empty public constructor
@@ -23,7 +31,25 @@ public class ContactUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_us, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_contact_us, container, false);
+        ButterKnife.bind(this, rootView);
+
+        // OnClickListener when "General Enquiry" is clicked
+        mComposeEmailTv.setOnClickListener(composeEmailOCL);
+
+        return rootView;
     }
+
+    private View.OnClickListener composeEmailOCL = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:" + getString(R.string.email_address_general_enquiry))); // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject_general_enquiry));
+            if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+    };
 
 }
