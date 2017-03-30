d16cdf0938dd80b180dedf2403b97ba9960f2ee4
Anky <anky_au@hotmail.com>
2017-03-30 22:10:26 +1000

    feat: Create layout for LocationDetailFragment

---
+++ app/src/main/java/me/anky/scf/LocationDetailActivity.java
@@ -0,0 +1,13 @@
+package me.anky.scf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_detail);
+    }
+}
---
+++ app/src/main/java/me/anky/scf/LocationDetailFragment.java
@@ -0,0 +1,30 @@
+package me.anky.scf;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LocationDetailFragment extends Fragment {
+
+
+    public LocationDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_location_detail, container, false);
+        return rootView;
+    }
+
+}
